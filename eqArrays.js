d5ff947b067020b1b1d68fa79185a87473f284db
Sarah <sarahjreive@gmail.com>
2019-07-01 23:14:21 +0000

    refactored eqArrays to use recursion

--- eqArrays.js
+++ eqArrays.js
@@ -2,12 +2,21 @@ const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
+
+  for (let i = 0; i < arr1.length; i++) {  
     if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (eqArrays(arr1[i], arr2[i]) !== true) {
+         return false; 
+        }
+      } else {
+      return false; 
+      }   
+    } 
+  } 
   return true;
 };
+  
+console.log(eqArrays([1,[2,3],2], [1,[2,3],2]));
 
 module.exports = eqArrays;
