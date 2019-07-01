f6981d26bdc0897c856a11367eaede56d1e3a5ab
Sarah <sarahjreive@gmail.com>
2019-07-01 19:05:11 +0000

    Added index.js file with all functions

---
+++ index.js
@@ -0,0 +1,35 @@
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const assertArraysEqual = require("./assertArraysEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
+const flatten = require("./flatten")
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
+
+module.exports = {
+    head,
+    tail,
+    middle,
+    countLetters,
+    countOnly,
+    eqArrays,
+    eqObjects,
+    findKey,
+    assertArraysEqual,
+    assertEqual,
+    assertObjectsEqual,
+    flatten,
+    letterPositions,
+    map,
+    takeUntil,
+    without
+};
